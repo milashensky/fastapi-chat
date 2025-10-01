3bd2c3b3acf178a49aed9fe643a3efdc5316a50e
Aleksandr Provornov aka Sasha <sasha@semasoftware.com>
2025-10-01 19:52:58 +0300

    add recursive proxy support for stateRef

---
+++ frontend/app/utils/__tests__/stateRef.spec.jsx
@@ -0,0 +1,80 @@
+import { fireEvent, act } from '@testing-library/react'
+import { describeComponent } from '~/test/unit/componentTest'
+import { useStateRef } from '../stateRef'
+
+describeComponent('stateRef', ({ render }) => {
+    it('should update component on value change', () => {
+        const StubComponent = () => {
+            const state = useStateRef(0)
+            return (
+                <button onClick={() => state.current += 1}>
+                    state: { state.current }
+                </button>
+            )
+        }
+        const component = render(<StubComponent />)
+        const button = component.getByText('state: 0')
+        expect(button).toBeInTheDocument()
+        act(() => {
+            fireEvent.click(button)
+        })
+        expect(component.getByText('state: 1')).toBeInTheDocument()
+    })
+
+    it('should work well with nested object assignment', () => {
+        const StubComponent = () => {
+            const state = useStateRef({
+                prop1: {
+                    prop2: 0,
+                },
+            })
+            const getDisplayValue = (item) => item.prop1.prop2
+            return (
+                <button onClick={() => state.current.prop1.prop2 += 1}>
+                    state: { getDisplayValue(state.current) }
+                </button>
+            )
+        }
+        const component = render(<StubComponent />)
+        const button = component.getByRole('button')
+        expect(button.textContent).toContain('0')
+        act(() => {
+            fireEvent.click(button)
+        })
+        expect(button.textContent).toContain('1')
+    })
+
+    it('should support nested null change', () => {
+        const StubComponent = () => {
+            const state = useStateRef({
+                prop1: null,
+            })
+            const getDisplayValue = (item) => JSON.stringify(item.prop1)
+            const toggleNested = () => {
+                if (state.current.prop1) {
+                    state.current.prop1 = null
+                    return
+                }
+                state.current.prop1 = { nested: 1 }
+            }
+            return (
+                <button onClick={toggleNested}>
+                    state: { getDisplayValue(state.current) }
+                </button>
+            )
+        }
+        const component = render(<StubComponent />)
+        const button = component.getByRole('button')
+        expect(button.textContent).toContain('null')
+        act(() => {
+            fireEvent.click(button)
+        })
+        expect(button.textContent).toContain('1')
+        expect(button.textContent).not.toContain('null')
+        act(() => {
+            fireEvent.click(button)
+        })
+        expect(button.textContent).not.toContain('1')
+        expect(button.textContent).toContain('null')
+    })
+})
--- frontend/app/utils/stateRef.ts
+++ frontend/app/utils/stateRef.ts
@@ -6,6 +6,32 @@ export interface StateRef<T> {
 
 export type MaybeRef<T> = StateRef<T> | T
 
+const getProxyHandler = <T, Key extends keyof T & string>(setter: (value: T) => void) => ({
+    get(target: T, key: Key): unknown {
+        const prop = target[key]
+        if (typeof prop === 'object' && prop !== null) {
+            type PropType = typeof prop
+            const update = (value: PropType) => {
+                const updated = {
+                    ...target,
+                    [key]: value,
+                } as T
+                setter(updated)
+            }
+            return new Proxy(prop, getProxyHandler(update))
+        }
+        return prop
+    },
+    set(target: T, key: Key, value: T[Key]) {
+        const updated = {
+            ...target,
+            [key]: value,
+        }
+        setter(updated)
+        return true
+    }
+})
+
 export const useStateRef = <T>(initialValue: T): StateRef<T> => {
     const [stateValue, setState] = useState<T>(initialValue)
     return new Proxy(
@@ -13,7 +39,20 @@ export const useStateRef = <T>(initialValue: T): StateRef<T> => {
             current: stateValue,
         },
         {
-            get: () => stateValue,
+            get: <R extends StateRef<T>>(target: R, key: keyof R) => {
+                if (typeof stateValue !== 'object') {
+                    return stateValue
+                }
+                if (stateValue === null) {
+                    return stateValue
+                }
+                return new Proxy(
+                    stateValue,
+                    getProxyHandler((updated) => {
+                        setState(updated as T)
+                    }),
+                )
+            },
             set (obj, prop, newValue: T) {
                 setState(newValue)
                 return true
